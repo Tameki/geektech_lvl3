4f82643c6792700012d5f1bc05f590db93b41048
Askar <myrsabekov@gmail.com>
2018-11-07 09:49:23 +0600

    Add Application component

---
+++ app/src/main/java/com/geektech/architectureexample/ArchitectureExampleApp.java
@@ -0,0 +1,11 @@
+package com.geektech.architectureexample;
+
+import android.app.Application;
+
+// Created by askar on 11/6/18.
+public class ArchitectureExampleApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
